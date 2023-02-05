0f8d8bf4f61d39f5f6d82c1c5f77ae98d8abf76f
Daniel Liebert <daniel.liebert@gmail.com>
2023-02-04 23:22:53 -0300

    Configure root route and disable turbo for all forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get "/" => "devise/sessions#new"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "devise/sessions#new"
 end
