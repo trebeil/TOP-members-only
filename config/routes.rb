0f691e25a538cdb8dc69e5f857c1815d42b7b3ef
Daniel Liebert <daniel.liebert@gmail.com>
2023-02-06 17:33:31 -0300

    Make turbo and devise work together to show flash messages correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    delete "sessions", to: "posts#index"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
