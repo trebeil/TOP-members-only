e9074df8a2ddbd504c5c084924755b28fd884368
Daniel Liebert <daniel.liebert@gmail.com>
2023-02-06 12:06:29 -0300

    Define Posts controller actions and views with signed in condition

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to :root
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params[:post][:user_id] = current_user.id
+    params.require(:post).permit(:title, :body, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
-  devise_for :users
-
-  devise_scope :user do
-    get "/" => "devise/sessions#new"
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "devise/sessions#new"
+  root "posts#index"
 
+  devise_for :users
   resources :posts, only: [:new, :create, :index]
+
 end
